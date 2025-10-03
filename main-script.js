27a403d65a626070ac78173cfeb3c5c6500bb475
weilunchen210 <weilunchen210@gmail.com>
2025-10-03 16:34:08 +0800

    template: Total minutes listened page

--- main-script.js
+++ main-script.js
@@ -1,4 +1,3 @@
-
 const tab1 = document.getElementById("tab1")
 tab1.addEventListener("click", () => switchTab(1));
 
@@ -8,6 +7,12 @@ tab2.addEventListener("click", () => switchTab(2));
 const tab3 = document.getElementById("tab3")
 tab3.addEventListener("click", () => switchTab(3));
 
+const tab4 = document.getElementById("tab4")
+tab4.addEventListener("click", () => switchTab(4));
+
+const tab5 = document.getElementById("tab5")
+tab5.addEventListener("click", () => switchTab(5));
+
 function switchTab(tab){
     switch(tab){
         case 1:
@@ -15,18 +20,40 @@ function switchTab(tab){
             tab1.classList.add("tab-active")
             tab2.classList.remove("tab-active")
             tab3.classList.remove("tab-active")
+            tab4.classList.remove("tab-active")
+            tab5.classList.remove("tab-active")
             break;
         case 2:
             console.log("2")
             tab1.classList.remove("tab-active")
             tab2.classList.add("tab-active")
             tab3.classList.remove("tab-active")
+            tab4.classList.remove("tab-active")
+            tab5.classList.remove("tab-active")
             break;
         case 3:
             console.log("3")
             tab1.classList.remove("tab-active")
             tab2.classList.remove("tab-active")
             tab3.classList.add("tab-active")
+            tab4.classList.remove("tab-active")
+            tab5.classList.remove("tab-active")
+            break;
+        case 4:
+            console.log("4")
+            tab1.classList.remove("tab-active")
+            tab2.classList.remove("tab-active")
+            tab3.classList.remove("tab-active")
+            tab4.classList.add("tab-active")
+            tab5.classList.remove("tab-active")
+            break;
+        case 5:
+            console.log("5")
+            tab1.classList.remove("tab-active")
+            tab2.classList.remove("tab-active")
+            tab3.classList.remove("tab-active")
+            tab4.classList.remove("tab-active")
+            tab5.classList.add("tab-active")
             break;
     }
 }
